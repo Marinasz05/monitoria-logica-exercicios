09cf54d14befa0947d688ec4fbaf302456991a5e
Marinasz05 <conceicao.marina71@dinfor.uniceub>
2025-09-10 18:45:47 -0300

    Resolucao

--- 03.py
+++ 03.py
@@ -6,3 +6,10 @@
     - Após isso, o programa deve verificar se o número digitado é
       par ou ímpar.
 """
+numero = int(input('Digite um número inteiro: '))
+
+if numero % 2 ==0 :
+    print('Esse número é par')
+
+else :
+    print('Esse número é ímpar')
